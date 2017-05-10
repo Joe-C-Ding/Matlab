998166281d4a644cbc263c5109bb2167b5f8f457
Joe Ding <dr_hqxz@126.com>
2017-05-10 18:14:34 +0800

    add plot_qy_single

--- calibration/plot_QY_single.m
+++ calibration/plot_QY_single.m
@@ -1,19 +1,14 @@
 start_tic = tic;
-close all;
+close all
 
-listing = dir('rainflow\*.mat');
-listing = {listing.name}';
+file = 'rainflow\empty\2014-07-27 08-32-45 (1).mat';
+load(file);
 
 Qedge = 0:0.5:120;
 Yedge = -30:0.25:30;
 [Y, Q] = meshgrid(Yedge, Qedge);
 
-QYsum = zeros(length(Qedge), length(Yedge), 2);
-for j = 1:length(listing)
-    load(['rainflow\', listing{j}]);
-    
-    QYsum = QYsum + QY;
-end
+QYsum = QY;
 QYsum(QYsum < 1e-5) = 0;
 
 for i = 1:2
@@ -30,9 +25,27 @@ for i = 1:2
     colorbar;
     
     xlim([-5 5]);
-    ylim([55 80]);
+    ylim([40 60]);
     xlabel(['Y', num2str(i), ' (kN)']);
     ylabel(['Q', num2str(i), ' (kN)']);
+end
+
+X = {Yedge, Qedge};
+s = {{'Y1', 'Y2'}, {'Q1', 'Q2'}};
+for i = 1:2
+    figure(i+2)
+    ax = gca;
+    hold(ax, 'on');
+    grid(ax, 'on');
+    
+    Y = sum(QYsum, i);
+    plot(ax, X{i}, Y(:,:,1), 'linewidth', 2);
+    plot(ax, X{i}, Y(:,:,2), 'linewidth', 2);
+    
+    ax.YScale = 'log';
+%     ax.YLim = [1 1e5];
+    
+    legend(s{i}, 'FontSize', 12);
 end
 
 fprintf('%s elapsed: %f s\n', mfilename, toc(start_tic));
